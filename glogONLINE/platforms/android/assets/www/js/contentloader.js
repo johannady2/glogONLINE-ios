def85729a617e87454702878557bc4dd8d78125d
johannady2 <Wachuy@Waynes-MacBook-Pro.local>
2015-06-04 16:25:43 +0800

    fdfsdfdsfds

---
+++ glogONLINE/platforms/android/assets/www/js/contentloader.js
@@ -0,0 +1,48 @@
+ $(document).ready(function()
+{ 
+      
+    
+       var defaultContent = 'splash.html';     
+         $(".content-cont").load(defaultContent,  null, function(event,filename)
+         {
+			 $('.site-content').removeClass('container');
+             $('body').css('background-image', 'url(' + '"img/splashbg.jpg"' + ')');
+			 $('.content-cont').css( 'marginTop','0px' );
+    		 $('.content-cont').css('marginBottom', '0px' );
+			
+
+			 
+			 
+				 $(".slideToUnlock").on('click',function()
+				 {
+
+					$('.site-content').addClass('container');
+					 $('body').css('background-image', 'none');
+					  $('.content-cont').css('marginTop','60px');
+						$('.content-cont').css('marginBottom','60px');
+					 
+					 	$('.content-cont').append('<p>You have\'nt scanned anything. <a href="#"  onclick="scanner.startScanning(MWBSInitSpace.init,MWBSInitSpace.callback)">Click here</a> to scan an item.</p>');
+
+
+
+
+
+					 $('nav , footer').show();
+					 $('.splashscreencont').remove();
+		
+
+					scanner.startScanning(MWBSInitSpace.init,MWBSInitSpace.callback);
+
+				 });
+         });
+  
+
+  
+    
+
+     
+ });
+
+
+ 
+         
