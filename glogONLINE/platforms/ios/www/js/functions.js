4e0f300b1e9cc475d9946c593446f7a859e09be7
johannady2 <Wachuy@Waynes-MacBook-Pro.local>
2015-06-13 19:30:22 +0800

    sadsadasd

--- glogONLINE/platforms/ios/www/js/functions.js
+++ glogONLINE/platforms/ios/www/js/functions.js
@@ -1,21 +1,41 @@
 	var networkstatus = '';
-
+    var ref = null;
 	var onlineSingleItemPictureFileName;
 	var onlineSingleItemBarcode;
 	var onlineSingleItemBrand;
 	var onlineSingleItemFullDescription;
 	var onlineSingleItemPromoName;
 	var onlineSingleItemPromoPrice;
+    var stocksavailable;   
+    var onlineSingleItemStocksAvailable;
+
+
+
+    var timerId;
+    setTimeout(function()
+    {
+
+
+    timerId = setInterval(function(){  bugFix(); }, 1800);
+
+    }, 3000);
+
+
+
+
+
+
 
 	if(scanResultWhenOffline == null)//initialize when not initialized
 	{
 		var scanResultWhenOffline;
 	}
 
-	var ref;
 
 
-	function onBodyLoad()
+
+
+    function onBodyLoad()
 	{   
 		document.addEventListener("offline", onDeviceOffline, false);
 		document.addEventListener("online", onDeviceOnline, false);
@@ -41,9 +61,15 @@
 
 	}
 
+                                                                                    
+
 
 	function onDeviceOnline()
 	{ 
+        
+
+
+        
 		networkstatus = 'connected';
 		if(scanResultWhenOffline != null)
 		{
@@ -55,8 +81,55 @@
 		$('.splashscreencont').show();
 		$('.splashloading').hide();
 		$('.slideToUnlock').show();
+        
+
+        
+	}
+
+function Exit()
+{
+              navigator.notification.confirm(
+                'Do you want to exit app?',  
+                function(i){
+                    if(i==2)
+                     {
+                       navigator.app.exitApp(); //This will Close the App
+                     }
+                },              
+                'App Name',            
+                'Cancel,Exit'          
+              );
+ }
+
+function bugFix()//sometimes noti popups don't appear so we check it and make them appear.
+{
+
+	if($('.splashpageindicator').is(":visible"))
+	{
+		if(($('.slideToUnlock').is(":visible") == false) && ($('.splashloading').is(":visible")== false))
+		{
+			
+			
+			if(($('.noti-offline').is(":hidden") == true) && ($('.noti-online').is(":hidden") == true))
+			{
+				
+				if(networkstatus !='disconnected' && networkstatus !='connected')
+				{
+					location.reload();
+				}
+			}
+			
+		}
+		else
+		{
+			$('.noti-offline').hide();
+			$('.noti-online').hide();
+		}
+		
+		
 
 	}
+}
 
 
 	/*----------------------------------------------------------------------*/
@@ -124,6 +197,19 @@
 												{
 													 onlineSingleItemPromoPrice = val[i];
 												}
+                                                else if(i == 'Stock_InvtyCat')
+                                                {
+                                                    onlineSingleItemStocksAvailable = val[i];
+                                                }
+                                               /* else if(i == 'PromoStartDate_CatMstr')
+                                                {
+                                                    onlineSingleItemPromoStartDate = val[i];
+                                                }
+                                                else if(i == 'PromoEndDate_CatMstr')
+                                                {
+                                                    onlineSingleItemPromoEndDate = val[i];
+                                                }*/
+                                               
 
 										});	
 
@@ -143,6 +229,8 @@
 					{
 						$(".content-cont").unload().load('online-single-item.html',  null, function()
 						{	
+                            //alert(onlineSingleItemPromoStartDate);
+                            //alert(onlineSingleItemPromoEndDate);
 							$('.addToPrestaCart').attr('data-barcode',onlineSingleItemBarcode);
 							$('.onlineSingleItemPromoPrice').append(onlineSingleItemPromoPrice);
 							$('.glogtotal').append(onlineSingleItemPromoPrice);
@@ -150,7 +238,20 @@
 							$('.onlineSingleItemFullDescription').append(onlineSingleItemFullDescription);
 							$('.onlineSingleItemBrand').append(onlineSingleItemBrand);
 							$('.onlineSingleItemPromoName').append(onlineSingleItemPromoName);
+							$('.onlineSingleItemStocksAvailable').append(onlineSingleItemStocksAvailable);
+                           
+                            if(onlineSingleItemStocksAvailable <= 0)
+                            {
+                                $('.addToPrestaCart').after('<p class="warning">Item out of stock</p>');
+                                $('.addToPrestaCart').hide();
+                            }
+                            
+                            setTimeout(function()
+                            {
+                                 $('.content-cont').empty();
+                                $('.content-cont').append('<p>Time Out. Please Scan Again.  <a href="#" onclick="scanner.startScanning(MWBSInitSpace.init,MWBSInitSpace.callback)">Click here</a></p>');
 
+                            }, 60000);
 
 							/*because when item is not available, variables are not updated which causes the last avaialble item to appear on online-single-item.html... By assigning them with '' value, I can output, "iteme unavailable" when value is '' item is not available according to the api*/
 							onlineSingleItemPictureFileName = '';
@@ -159,6 +260,10 @@
 							onlineSingleItemFullDescription = '';
 							onlineSingleItemPromoName = '';
 							onlineSingleItemPromoPrice = '';
+                            stocksavailable =  onlineSingleItemStocksAvailable;
+                            onlineSingleItemStocksAvailable = '';
+                            
+                            
 						});
 
 
@@ -181,8 +286,6 @@
 	$(document).on('input','#onlineSingleItemEnteredQuantity',function ()
 	{
 		/*keycodes undefined are undefined so i did this instead*/
-
-
 		var glogprice = $('.onlineSingleItemPromoPrice').html(); 
 
 		 var currentvalue = $('#onlineSingleItemEnteredQuantity').val();
@@ -190,14 +293,39 @@
 
 		//alert('glogqlen ='+glogqlen);
 
-		if(glogqlen.length>0 && currentvalue != 0 && currentvalue !='0' && testinput(/[^0-9.]/, currentvalue)==0)//if not empty && not zero && (does not contain any none numeric)
+		if(glogqlen.length>0 && currentvalue != 0 && currentvalue !='0' && testinput(/[^0-9]/, currentvalue)==0)//if not empty && not zero && (does not contain any none numeric)
 		{
 		  //alert('in if');
 			//alert('currentvalue =' + currentvalue);
+            
+            var newvalue = currentvalue.toString().replace(/[^0-9]+/g, '');
+	    
+		
+            
 
-			var newvalue = currentvalue.toString().replace(/[^0-9\.]+/g, '');
-			$('#onlineSingleItemEnteredQuantity').val(newvalue);
-			var qval = $('#onlineSingleItemEnteredQuantity').val();
+            if(parseInt(newvalue) > parseInt(stocksavailable))
+            {
+                $('#onlineSingleItemEnteredQuantity').blur();
+                
+                $('.noti-any , .noti-blanket').show();
+                $('.noti-any').empty();
+                $('.noti-any').append('There are only  ' + stocksavailable + '  stocks left');
+                
+                
+                
+                setTimeout(function()
+                {
+                     $('.noti-any , .noti-blanket').hide();
+                }, 1500);
+                               
+               $('#onlineSingleItemEnteredQuantity').val(stocksavailable); 
+            }
+            else
+            {   $('#onlineSingleItemEnteredQuantity').val(newvalue);
+			    
+            }
+            
+            var qval = $('#onlineSingleItemEnteredQuantity').val();
 
 
 		}
@@ -208,9 +336,11 @@
 			currentvalue = 1;
 		   // //alert('currentvalue =' + currentvalue);
 
-			var newvalue = currentvalue.toString().replace(/[^0-9\.]+/g, '');
+			var newvalue = currentvalue.toString().replace(/[^0-9]+/g, '');
+            
 			$('#onlineSingleItemEnteredQuantity').val('');
 			var qval = 1;
+            
 
 		}
 
@@ -257,8 +387,27 @@
 	$('body').on('click','.addToPrestaCart',function()
 	{
 
-		ref = window.open('http://viveg.net/index.php?barcode='+$(this).attr('data-barcode')+'&quantity='+$(this).attr('data-quantity')+'&localmobiledate='+getDateNow()+'&glog-app-access=76ef0d45220fdee3ac883a0c7565e50c', '_blank', 'location=yes');
+		ref = window.open('http://viveg.net/index.php?barcode='+$(this).attr('data-barcode')+'&quantity='+$(this).attr('data-quantity')+'&localmobiledate='+getDateNow()+'&glog-app-access=76ef0d45220fdee3ac883a0c7565e50c', '_blank', 'location=no');
+     //   ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
+     //   ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
+      //  ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
+     //   ref.addEventListener('exit', function(event) { alert(event.type); });
+
+
 	});
+
+    $('body').on('click','.viewOnlineCart',function()
+    {
+    
+    
+        ref = window.open('http://viveg.net/index.php?controller=order&glog-app-access=76ef0d45220fdee3ac883a0c7565e50c', '_blank', 'location=no');
+       //         ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
+     //   ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
+      //  ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
+     //   ref.addEventListener('exit', function(event) {  alert(event.type); });
+        
+
+    });
 
 	/*-----------------------------------------------------------------------*/
 	/*-------------------//online-single-item.html-------------------------------*/
@@ -286,7 +435,7 @@
 
 $('.content-cont').bind("DOMSubtreeModified",function()
 {
-
+//hide toggle menu if back button is visible
     
         if($(document).width() <= 767)
         {
@@ -306,3 +455,7 @@ $('.content-cont').bind("DOMSubtreeModified",function()
 
  
 });
+
+
+
+
